8eecb71992e3da776f89733a9ad369b262572b2a
erick <estoledo1@utpl.edu.ec>
2023-11-08 16:32:59 -0500

    actuality

--- ejemplos/Ejemplo3/src/ejemplo3/Ejemplo3.java
+++ ejemplos/Ejemplo3/src/ejemplo3/Ejemplo3.java
@@ -30,8 +30,15 @@ public class Ejemplo3 {
                 System.out.printf("Estudiante en suspenso con un "
                         + "promedio: %.2f\n", promedio);
             } else {
-                System.out.printf("Estudiante reprobado con un "
-                        + "promedio: %.2f\n", promedio);
+
+                if (promedio >= 3 && promedio < 5) {
+                    System.out.printf("estudiante reprobado con "
+                            + "un promedio: %.2f/n", promedio);
+                } else {
+                    System.out.printf("estudiante recuperacion con "
+                            + "un promedio: %.2f/n", promedio);
+                }
+
             }
 
         }
